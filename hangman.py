cfef95ce6b2d365f6317946f74feedbaea711e71
TThibeau <91048229+TThibeau@users.noreply.github.com>
2022-03-26 20:48:05 +0100

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -14,41 +14,39 @@ print(banner)
 
 display = []
 
-for letter in chosen_word:
+for _ in chosen_word:
     display.append("_")
 
 print(' '.join(display) + "\n")
 
-def add_to_word(guess):
-    if guess in chosen_word:
-        if guess not in ''.join(display):
+def add_to_word(guessed_letter):
+    if guessed_letter in chosen_word:
+        if guessed_letter not in ''.join(display):
             print("\nCorrect, the letter has been added!\n")
         else:
             print("\nYou have already guessed this letter before\n")
 
     for pos in range(len(chosen_word)):
-        letter = chosen_word[pos]
-        if guess == letter:
-            display[pos] = letter
+        if guessed_letter == chosen_word[pos]:
+            display[pos] = guessed_letter
 
-def add_to_hangman(guess,lives):
-    if guess not in chosen_word:
-        lives -= 1
+def add_to_hangman(guessed_letter,nr_lives):
+    if guessed_letter not in chosen_word:
+        nr_lives -= 1
         print("\nWrong, you have lost a life!\n")   
-    return lives
+    return nr_lives
 
 def check_game_over():
     if ''.join(display) == chosen_word:
         print("You win!")
-        game_over = True
+        return True
 
     elif lives == 0:
         print("You lose!\n")
         print(f"The correct word was {chosen_word}")
-        game_over = True
+        return True
     else:
-        game_over = False
-    return game_over
+        return False
 
 def clear():
      os.system('cls' if os.name=='nt' else 'clear')
